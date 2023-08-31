f7748dd10a4a966929911dc6d70d1f5c712c456d
KimMinheee <99alsgmlalsl@naver.com>
2023-08-31 16:45:52 +0900

    [Feat] : 프로젝트 세팅

---
+++ src/main/java/com/daangn/dangunmarket/DangunmarketApplication.java
@@ -0,0 +1,13 @@
+package com.daangn.dangunmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangunmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DangunmarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daangn/dangunmarket/DangunmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daangn.dangunmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DangunmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
