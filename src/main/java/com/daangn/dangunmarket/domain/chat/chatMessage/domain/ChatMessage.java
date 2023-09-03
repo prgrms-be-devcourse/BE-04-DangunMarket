f8f9ddef4fa7c93a5f89a742ebe481387c0fb5f9
KimMinheee <99alsgmlalsl@naver.com>
2023-09-03 14:23:12 +0900

    [Feat] : Chat 도메인 관련 Entity 구현

---
+++ src/main/java/com/daangn/dangunmarket/domain/chat/chatMessage/domain/ChatMessage.java
@@ -0,0 +1,38 @@
+package com.daangn.dangunmarket.domain.chat.chatMessage.domain;
+
+import com.daangn.dangunmarket.domain.chat.chatRoomInfo.domain.ChatRoomInfo;
+import com.daangn.dangunmarket.domain.member.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "chat_messages")
+@NoArgsConstructor
+public class ChatMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private boolean readOrNot;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_infos_id", referencedColumnName = "id", nullable = false)
+    private ChatRoomInfo chatRoomInfo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "members_id", referencedColumnName = "id", nullable = false)
+    private Member member;
+
+    public ChatMessage(String content, boolean readOrNot, ChatRoomInfo chatRoomInfo, Member member) {
+        this.content = content;
+        this.readOrNot = readOrNot;
+        this.chatRoomInfo = chatRoomInfo;
+        this.member = member;
+    }
+
+}
