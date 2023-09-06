0aa282eae9251d5bcf4a241823ce20b6234080db
byeolhaha <j35635aa@gmail.com>
2023-09-06 14:08:07 +0900

    Feat : JWT Custom 필터 구현

---
+++ src/main/java/com/daangn/dangunmarket/domain/auth/jwt/AuthTokenProvider.java
@@ -0,0 +1,87 @@
+package com.daangn.dangunmarket.domain.auth.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+public class AuthTokenProvider {
+
+    @Value("${app.auth.accessTokenExpiry}")
+    private String accessTokenExpiry;
+
+    @Value("${app.auth.refreshTokenExpiry}")
+    private String refreshTokenExpiry;
+
+    private final Key key;
+    private static final String AUTHORITIES_KEY = "role";
+
+    public AuthTokenProvider(@Value("${app.auth.tokenSecret}") String secretKey) {
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    public AuthToken createAuthToken(String id, String role, String expiry, Long memberId) {
+        Date accessExpiryDate = getExpiryDate(expiry);
+        return AuthToken.builder()
+                .socialId(id)
+                .memberId(memberId)
+
+                .role(role)
+                .expiry(accessExpiryDate)
+
+                .key(key)
+
+                .build();
+    }
+
+    public AuthToken createUserAppToken(String id, Long memberId) {
+        return createAuthToken(id, "ROLE_USER", accessTokenExpiry, memberId);
+    }
+
+    public AuthToken createUserRefreshToken(String id, Long memberId) {
+        return createAuthToken(id, "ROLE_USER", refreshTokenExpiry, memberId);
+    }
+
+    public AuthToken convertAuthToken(String token) {
+        return new AuthToken(token, key);
+    }
+
+    public static Date getExpiryDate(String expiry) {
+        return new Date(System.currentTimeMillis() + Long.parseLong(expiry));
+    }
+
+    public Authentication getAuthentication(AuthToken authToken) {
+
+        Claims claims = authToken.getTokenClaims();
+        if (claims == null) {
+            return null;
+        }
+
+        List<SimpleGrantedAuthority> authorities = Arrays.stream(new String[]{claims.get(AUTHORITIES_KEY).toString()})
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+
+        int memberId = (int) claims.get("memberId");
+
+        CustomUser customUser = CustomUser.builder()
+                .socialId(claims.getSubject())
+                .memberId((long) memberId)
+                .authorities(authorities)
+                .build();
+
+        return new UsernamePasswordAuthenticationToken(customUser, authToken, authorities);
+    }
+
+}
