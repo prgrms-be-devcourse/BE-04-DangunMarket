d3c9486d427ca8a84cf63c43b415d99d45b8b79d
young970 <yyww9@naver.com>
2023-09-07 12:50:33 +0900

    feat: createProduct 컨트롤러dto validation 추가

--- src/main/java/com/daangn/dangunmarket/domain/product/controller/dto/ProductCreateApiRequest.java
+++ src/main/java/com/daangn/dangunmarket/domain/product/controller/dto/ProductCreateApiRequest.java
@@ -1,5 +1,7 @@
 package com.daangn.dangunmarket.domain.product.controller.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.Serializable;
@@ -7,15 +9,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public record ProductCreateApiRequest(
-        Long memberId,
-        Long areaId,
-        Double latitude,
-        Double longitude,
-        String alias,
+        @NotNull(message = "memberId가 누락되었습니다.") Long memberId,
+        @NotNull(message = "areaId가 누락되었습니다.") Long areaId,
+        @NotNull(message = "위도가 누락되었습니다.") Double latitude,
+        @NotNull(message = "경도가 누락되었습니다.") Double longitude,
+        @NotNull(message = "alias가 누락되었습니다.") String alias,
         List<MultipartFile> files,
         Long categoryId,
-        String title,
-        String content,
+        @NotBlank(message = "title이 누락되었거나 비었습니다.") String title,
+        @NotBlank(message = "content가 누락되었거나 비었습니다.") String content,
         Long price,
         boolean isOfferAllowed,
         LocalDateTime refreshedAt) implements Serializable {
