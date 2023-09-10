51ff67c66f5a7f598cf711516bb35c03b3c3cb9a
KimMinhee <99alsgmlalsl@naver.com>
2023-09-10 17:40:33 +0900

    [Refactor] : ErrorResponse 내 innerClass 명 수정 - FieldError -> FieldDetailError

--- src/main/java/com/daangn/dangunmarket/global/response/ErrorResponse.java
+++ src/main/java/com/daangn/dangunmarket/global/response/ErrorResponse.java
@@ -18,11 +18,11 @@ import java.util.stream.Collectors;
 public class ErrorResponse {
     private String code; //서버 내 에러코드
     private String message; //에러 메시지
-    private List<FieldError> fieldErrors; //상세 에러 메시지
+    private List<FieldDetailError> fieldDetailErrors; //상세 에러 메시지
     private String reason; //에러 이유 - Exception 정보
 
     public static ErrorResponse of(final ErrorCode errorCode, final BindingResult bindingResult) {
-        return new ErrorResponse(errorCode, FieldError.of(bindingResult));
+        return new ErrorResponse(errorCode, FieldDetailError.of(bindingResult));
     }
 
     public static ErrorResponse of(ErrorCode errorCode) {
@@ -36,7 +36,7 @@ public class ErrorResponse {
     private ErrorResponse(final ErrorCode errorCode) {
         this.code = errorCode.getCode();
         this.message = errorCode.getMessage();
-        this.fieldErrors = new ArrayList<>();
+        this.fieldDetailErrors = new ArrayList<>();
     }
 
     private ErrorResponse(final ErrorCode errorCode, final String reason) {
@@ -45,28 +45,28 @@ public class ErrorResponse {
         this.reason = reason;
     }
 
-    private ErrorResponse(final ErrorCode errorCode, final List<FieldError> fieldErrors) {
+    private ErrorResponse(final ErrorCode errorCode, final List<FieldDetailError> fieldDetailErrors) {
         this.code = errorCode.getCode();
         this.message = errorCode.getMessage();
-        this.fieldErrors = fieldErrors;
+        this.fieldDetailErrors = fieldDetailErrors;
     }
 
-    public static class FieldError {
+    public static class FieldDetailError {
         private final String field;
         private final String value;
         private final String reason;
 
-        private static List<FieldError> of(final BindingResult bindingResult) {
+        private static List<FieldDetailError> of(final BindingResult bindingResult) {
             final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
             return fieldErrors.stream()
-                    .map(error -> new FieldError(
+                    .map(error -> new FieldDetailError(
                             error.getField(),
                             error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
                             error.getDefaultMessage()))
                     .collect(Collectors.toList());
         }
 
-        private FieldError(String field, String value, String reason) {
+        private FieldDetailError(String field, String value, String reason) {
             this.field = field;
             this.value = value;
             this.reason = reason;
