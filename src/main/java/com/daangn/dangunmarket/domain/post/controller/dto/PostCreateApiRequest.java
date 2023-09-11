406abb9ec7400212ac08615a2af43de0464070d3
young970 <yyww9@naver.com>
2023-09-12 03:57:08 +0900

    [Feat] : createPost 시 업로드 가능한 파일 개수 제한 컨트롤러의 DTO에 추가

--- src/main/java/com/daangn/dangunmarket/domain/post/controller/dto/PostCreateApiRequest.java
+++ src/main/java/com/daangn/dangunmarket/domain/post/controller/dto/PostCreateApiRequest.java
@@ -3,6 +3,7 @@ package com.daangn.dangunmarket.domain.post.controller.dto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.Serializable;
@@ -14,7 +15,7 @@ public record PostCreateApiRequest(
         @NotNull(message = "위도가 누락되었습니다.") Double latitude,
         @NotNull(message = "경도가 누락되었습니다.") Double longitude,
         @NotNull(message = "alias가 누락되었습니다.") String alias,
-        List<MultipartFile> files,
+        @Size(max = 3, message = "파일은 최대 3개까지만 업로드 가능합니다.") List<MultipartFile> files,
         Long categoryId,
         @NotBlank(message = "title이 누락되었거나 비었습니다.") String title,
         @NotBlank(message = "content가 누락되었거나 비었습니다.") String content,
