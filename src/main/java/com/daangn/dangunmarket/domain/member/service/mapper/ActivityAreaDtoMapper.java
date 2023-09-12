edf990875ebf39ef9f4ac389142ceeccd31d9486
byeolhaha <j35635aa@gmail.com>
2023-09-12 14:53:33 +0900

    [Refactor] : Mapstruct bean 등록 관련 중복 기능 삭제

--- src/main/java/com/daangn/dangunmarket/domain/member/service/mapper/ActivityAreaDtoMapper.java
+++ src/main/java/com/daangn/dangunmarket/domain/member/service/mapper/ActivityAreaDtoMapper.java
@@ -3,13 +3,17 @@ package com.daangn.dangunmarket.domain.member.service.mapper;
 import com.daangn.dangunmarket.domain.member.model.ActivityArea;
 import com.daangn.dangunmarket.domain.member.service.dto.ActivityAreaCreateResponse;
 import com.daangn.dangunmarket.domain.member.service.dto.ActivityAreaFindResponse;
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring")
+@Mapper(
+        componentModel = "spring",
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR
+)
 public interface ActivityAreaDtoMapper {
-    ActivityAreaDtoMapper INSTANCE = Mappers.getMapper(ActivityAreaDtoMapper.class);
-
+    @Mapping(target = "isVerified", ignore = true)
     ActivityAreaFindResponse toActivityAreaFindResponse(ActivityArea activityArea);
 
 }
--- src/main/java/com/daangn/dangunmarket/domain/member/service/mapper/MemberDtoMapper.java
+++ src/main/java/com/daangn/dangunmarket/domain/member/service/mapper/MemberDtoMapper.java
@@ -3,13 +3,15 @@ package com.daangn.dangunmarket.domain.member.service.mapper;
 import com.daangn.dangunmarket.domain.member.model.Member;
 import com.daangn.dangunmarket.domain.member.service.dto.MemberCreateResponse;
 import com.daangn.dangunmarket.domain.member.service.dto.MemberFindResponse;
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring")
+@Mapper(
+        componentModel = "spring",
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR
+)
 public interface MemberDtoMapper {
-    MemberDtoMapper INSTANCE = Mappers.getMapper(MemberDtoMapper.class);
-
     MemberFindResponse toMemberFindResponse(Member member);
 
     MemberCreateResponse toMemberCreateResponse(Member member);
