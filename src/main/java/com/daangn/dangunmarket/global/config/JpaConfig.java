b3b16fda124552471f3941c7b5a775936c939b09
KimMinhee <99alsgmlalsl@naver.com>
2023-09-13 03:44:48 +0900

    [Refactor] : QueryDslConfig 파일 코드 수정

--- src/main/java/com/daangn/dangunmarket/global/config/JpaConfig.java
+++ src/main/java/com/daangn/dangunmarket/global/config/JpaConfig.java
@@ -1,18 +1,10 @@
 package com.daangn.dangunmarket.global.config;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaConfig {
-
-    @Bean
-    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
-        return new JPAQueryFactory(entityManager);
-    }
 
 }
