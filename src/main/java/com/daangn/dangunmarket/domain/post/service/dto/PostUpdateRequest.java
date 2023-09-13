2d3de275921d69b697281aabe2bed2344fc036fc
byeolhaha <j35635aa@gmail.com>
2023-09-14 00:47:01 +0900

    [Feat] : 게시글 수정을 위한 dto 구현

---
+++ src/main/java/com/daangn/dangunmarket/domain/post/service/dto/PostUpdateRequest.java
@@ -0,0 +1,20 @@
+package com.daangn.dangunmarket.domain.post.service.dto;
+
+import com.daangn.dangunmarket.domain.post.model.Category;
+import com.daangn.dangunmarket.domain.post.model.LocationPreference;
+import com.daangn.dangunmarket.domain.post.model.PostImage;
+
+import java.util.List;
+
+public record PostUpdateRequest (
+        Long postId,
+        LocationPreference locationPreference,
+        List<PostImage> postImages,
+        Category category,
+        Long areaId,
+        String title,
+        String content,
+        long price,
+        boolean isOfferAllowed) {
+}
+
