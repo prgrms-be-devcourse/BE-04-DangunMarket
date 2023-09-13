a79cdd905f24ce5f96bfe9b08b77471cbec3e83c
byeolhaha <j35635aa@gmail.com>
2023-09-14 00:49:49 +0900

    [Feat] : 게시글 수정 Mapper 구현

--- src/main/java/com/daangn/dangunmarket/domain/post/facade/mpper/PostParamMapper.java
+++ src/main/java/com/daangn/dangunmarket/domain/post/facade/mpper/PostParamMapper.java
@@ -1,12 +1,14 @@
 package com.daangn.dangunmarket.domain.post.facade.mpper;
 
 import com.daangn.dangunmarket.domain.post.facade.dto.PostCreateRequestParam;
+import com.daangn.dangunmarket.domain.post.facade.dto.PostUpdateRequestParam;
 import com.daangn.dangunmarket.domain.post.facade.dto.PostsGetResponseParam;
 import com.daangn.dangunmarket.domain.post.model.Category;
 import com.daangn.dangunmarket.domain.post.model.LocationPreference;
 import com.daangn.dangunmarket.domain.post.model.PostImage;
 import com.daangn.dangunmarket.domain.post.service.dto.PostCreateRequest;
 import com.daangn.dangunmarket.domain.post.service.dto.PostGetResponses;
+import com.daangn.dangunmarket.domain.post.service.dto.PostUpdateRequest;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -26,5 +28,12 @@ public interface PostParamMapper {
 
     @Mapping(source = "areaName", target = "userAreaName")
     PostsGetResponseParam toPostsGetResponseParam(String areaName, PostGetResponses responsePage);
+
+    PostUpdateRequest toPostUpdateRequest(
+            PostUpdateRequestParam request,
+            LocationPreference locationPreference,
+            List<PostImage> postImages,
+            Category category,
+            Long areaId);
 
 }
