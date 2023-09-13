20f157a33a403c2fd192d07cf9b7b6e918b97c5a
KimMinhee <99alsgmlalsl@naver.com>
2023-09-14 04:26:15 +0900

    [Feat] : 게시글 검색 Facade 구현

--- src/main/java/com/daangn/dangunmarket/domain/post/facade/PostFacade.java
+++ src/main/java/com/daangn/dangunmarket/domain/post/facade/PostFacade.java
@@ -6,7 +6,9 @@ import com.daangn.dangunmarket.domain.member.service.MemberService;
 import com.daangn.dangunmarket.domain.member.service.dto.MemberFindResponse;
 import com.daangn.dangunmarket.domain.post.facade.dto.PostCreateRequestParam;
 import com.daangn.dangunmarket.domain.post.facade.dto.PostFindResponseParam;
-import com.daangn.dangunmarket.domain.post.facade.dto.PostsGetResponseParam;
+import com.daangn.dangunmarket.domain.post.facade.dto.PostGetResponseParams;
+import com.daangn.dangunmarket.domain.post.facade.dto.PostSearchRequestParam;
+import com.daangn.dangunmarket.domain.post.facade.dto.PostSearchResponseParams;
 import com.daangn.dangunmarket.domain.post.facade.mpper.PostParamMapper;
 import com.daangn.dangunmarket.domain.post.model.Category;
 import com.daangn.dangunmarket.domain.post.model.LocationPreference;
@@ -15,6 +17,8 @@ import com.daangn.dangunmarket.domain.post.service.CategoryService;
 import com.daangn.dangunmarket.domain.post.service.PostService;
 import com.daangn.dangunmarket.domain.post.service.dto.PostFindResponse;
 import com.daangn.dangunmarket.domain.post.service.dto.PostGetResponses;
+import com.daangn.dangunmarket.domain.post.service.dto.PostSearchConditionRequest;
+import com.daangn.dangunmarket.domain.post.service.dto.PostSearchResponses;
 import com.daangn.dangunmarket.global.GeometryTypeFactory;
 import com.daangn.dangunmarket.global.aws.s3.S3Uploader;
 import org.locationtech.jts.geom.Point;
@@ -77,12 +81,11 @@ public class PostFacade {
         return PostFindResponseParam.of(response, memberName, areaName);
     }
 
-    public PostsGetResponseParam getPosts(Long memberId, Pageable pageable) {
+    public PostGetResponseParams getPosts(Long memberId, Pageable pageable) {
         MemberFindResponse memberResponse = memberService.findById(memberId);
         if (!isMemberActivityAreaValid(memberResponse.activityAreas())) {
             throw new IllegalStateException("회원의 활동지역이 존재하지 않습니다.");
         }
-
         Long areaId = memberResponse
                 .activityAreas()
                 .get(0)
@@ -92,9 +95,30 @@ public class PostFacade {
                 .areaName();
 
         PostGetResponses postResponses = postService.getPosts(areaId, pageable);
-        PostsGetResponseParam postGetResponseParam = mapper.toPostsGetResponseParam(areaName, postResponses);
+        PostGetResponseParams postGetResponseParam = mapper.toPostsGetResponseParam(areaName, postResponses);
 
         return postGetResponseParam;
+    }
+
+    public PostSearchResponseParams getPostsByConditions(PostSearchRequestParam postSearchRequestParam, Long memberId) {
+        MemberFindResponse memberFindResponse = memberService.findById(memberId);
+        if (!isMemberActivityAreaValid(memberFindResponse.activityAreas())) {
+            throw new IllegalStateException("회원의 활동지역이 존재하지 않습니다.");
+        }
+        Long areaId = memberFindResponse
+                .activityAreas()
+                .get(0)
+                .getId();
+        String areaName = areaService
+                .findById(areaId)
+                .areaName();
+
+        PostSearchConditionRequest searchConditionRequest = mapper.toPostSearchConditionRequest(postSearchRequestParam);
+
+        PostSearchResponses responses = postService.searchPosts(areaId, searchConditionRequest);
+        PostSearchResponseParams params = mapper.toPostSearchResponseParams(areaName, responses);
+
+        return params;
     }
 
     private boolean isMemberActivityAreaValid(List<ActivityArea> activityAreas) {
--- src/main/java/com/daangn/dangunmarket/domain/post/facade/mpper/PostParamMapper.java
+++ src/main/java/com/daangn/dangunmarket/domain/post/facade/mpper/PostParamMapper.java
@@ -1,12 +1,16 @@
 package com.daangn.dangunmarket.domain.post.facade.mpper;
 
 import com.daangn.dangunmarket.domain.post.facade.dto.PostCreateRequestParam;
-import com.daangn.dangunmarket.domain.post.facade.dto.PostsGetResponseParam;
+import com.daangn.dangunmarket.domain.post.facade.dto.PostGetResponseParams;
+import com.daangn.dangunmarket.domain.post.facade.dto.PostSearchRequestParam;
+import com.daangn.dangunmarket.domain.post.facade.dto.PostSearchResponseParams;
 import com.daangn.dangunmarket.domain.post.model.Category;
 import com.daangn.dangunmarket.domain.post.model.LocationPreference;
 import com.daangn.dangunmarket.domain.post.model.PostImage;
 import com.daangn.dangunmarket.domain.post.service.dto.PostCreateRequest;
 import com.daangn.dangunmarket.domain.post.service.dto.PostGetResponses;
+import com.daangn.dangunmarket.domain.post.service.dto.PostSearchConditionRequest;
+import com.daangn.dangunmarket.domain.post.service.dto.PostSearchResponses;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -25,6 +29,10 @@ public interface PostParamMapper {
             Category category);
 
     @Mapping(source = "areaName", target = "userAreaName")
-    PostsGetResponseParam toPostsGetResponseParam(String areaName, PostGetResponses responsePage);
+    PostGetResponseParams toPostsGetResponseParam(String areaName, PostGetResponses responsePage);
 
+    PostSearchConditionRequest toPostSearchConditionRequest(PostSearchRequestParam postSearchRequestParam);
+
+    @Mapping(source = "areaName", target = "userAreaName")
+    PostSearchResponseParams toPostSearchResponseParams(String areaName, PostSearchResponses postSearchResponses);
 }
