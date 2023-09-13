561d585a3253b1e2128c70fe3473a9af65e92cd2
byeolhaha <j35635aa@gmail.com>
2023-09-13 17:37:58 +0900

    [Feat] : 수정을 요청한 회원과 게시글 작성자가 다른 경우 커스텀 예외 구현

--- src/main/java/com/daangn/dangunmarket/global/response/ErrorCode.java
+++ src/main/java/com/daangn/dangunmarket/global/response/ErrorCode.java
@@ -29,9 +29,10 @@ public enum ErrorCode {
     NOT_FOUND_POST_ENTITY("P002", "Post를 찾을 수 없습니다."),
     NOT_FOUND_POST_LIKE_ENTITY("P003", "Post의 좋아요 기록을 찾을 수 없습니다."),
     ALREADY_EXISTS_POST_LIKE("P004", "Post의 좋아요 기록이 이미 존재합니다."),
+    POST_NOT_CREATED_BY_USER("P005", "게시글을 작성한 회원이 아니므로 게시글을 수정할 수 없습니다."),
 
     //login
-    EXPIRED_TOKEN("L001", "토큰이 만료되었씁니다."),
+    EXPIRED_TOKEN("L001", "토큰이 만료되었습니다."),
     UNAUTHORIZED_TOKEN("L002", "인증되지 않은 토큰입니다."),
     OAUTH_CLIENT_SERVER_ERROR("L003", "oauth 클라이언트 서버 에러입니다.");
 
