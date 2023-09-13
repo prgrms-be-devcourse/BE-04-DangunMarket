e1fc51985eb7662edfef208b498a6ea420d37d82
byeolhaha <j35635aa@gmail.com>
2023-09-13 17:40:38 +0900

    [Feat] : 게시글 수정하기 위한 기존 글 불러오는 controller 구현

--- src/main/java/com/daangn/dangunmarket/domain/post/controller/PostController.java
+++ src/main/java/com/daangn/dangunmarket/domain/post/controller/PostController.java
@@ -1,31 +1,39 @@
 package com.daangn.dangunmarket.domain.post.controller;
 
+import com.daangn.dangunmarket.domain.auth.jwt.CustomUser;
 import com.daangn.dangunmarket.domain.post.controller.dto.PostCreateApiRequest;
 import com.daangn.dangunmarket.domain.post.controller.dto.PostCreateApiResponse;
 import com.daangn.dangunmarket.domain.post.controller.dto.PostFindApiResponse;
+import com.daangn.dangunmarket.domain.post.controller.dto.PostToUpdateApiResponse;
 import com.daangn.dangunmarket.domain.post.controller.mapper.PostApiMapper;
+import com.daangn.dangunmarket.domain.post.controller.mapper.PostUpdateApiMapper;
 import com.daangn.dangunmarket.domain.post.facade.PostFacade;
 import com.daangn.dangunmarket.domain.post.facade.dto.PostFindResponseParam;
+import com.daangn.dangunmarket.domain.post.facade.dto.PostToUpdateResponseParam;
+import com.daangn.dangunmarket.domain.post.service.dto.PostToUpdateResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
 @RestController
-@RequestMapping(value = "/api/posts",
+@RequestMapping(value = "/posts",
         consumes = MediaType.APPLICATION_JSON_VALUE,
         produces = MediaType.APPLICATION_JSON_VALUE)
 public class PostController {
 
-    private PostFacade postFacade;
-    private PostApiMapper mapper;
+    private final PostFacade postFacade;
+    private final PostApiMapper mapper;
+    private final PostUpdateApiMapper postUpdateApiMapper;
 
-    public PostController(PostFacade postFacade, PostApiMapper mapper) {
+    public PostController(PostFacade postFacade, PostApiMapper mapper, PostUpdateApiMapper postUpdateApiMapper) {
         this.postFacade = postFacade;
         this.mapper = mapper;
+        this.postUpdateApiMapper = postUpdateApiMapper;
     }
 
     @PostMapping
@@ -42,13 +50,21 @@ public class PostController {
         return ResponseEntity.created(uri).body(response);
     }
 
-
     @GetMapping("/{postId}")
     public ResponseEntity<PostFindApiResponse> findById(@PathVariable Long postId) {
         PostFindResponseParam responseParam = postFacade.findById(postId);
         PostFindApiResponse response = PostFindApiResponse.from(responseParam);
 
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{postId}/edit")
+    public ResponseEntity<PostToUpdateApiResponse> findPostInfoToUpdate(@PathVariable Long postId, Authentication authentication) {
+
+        CustomUser customUser = (CustomUser) authentication.getPrincipal();
+        PostToUpdateResponseParam postInfoToUpdate = postFacade.findPostInfoToUpdateById(customUser.memberId(), postId);
+
+        return ResponseEntity.ok(postUpdateApiMapper.toPostToUpdateApiResponse(postInfoToUpdate));
     }
 
 }
