76d5b1008708a5eeeafac9e0dac9e8a90fae6fee
byeolhaha <j35635aa@gmail.com>
2023-09-14 15:46:25 +0900

    [Feat] : 중복된 이미지, 새로운 이미지, 삭제될 이미지를  구분하는 이미지 수정 서비스 기능 구현

---
+++ src/main/java/com/daangn/dangunmarket/domain/post/service/PostImageService.java
@@ -0,0 +1,42 @@
+package com.daangn.dangunmarket.domain.post.service;
+
+import com.amazonaws.services.kms.model.NotFoundException;
+import com.daangn.dangunmarket.domain.post.model.Post;
+import com.daangn.dangunmarket.domain.post.model.PostImage;
+import com.daangn.dangunmarket.domain.post.repository.post.PostRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static com.daangn.dangunmarket.global.response.ErrorCode.NOT_FOUND_POST_ENTITY;
+
+@Transactional
+@Service
+public class PostImageService {
+
+    private final PostRepository postRepository;
+
+    public PostImageService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public List<PostImage> editImages(Long postId, List<PostImage> newPostImages) {
+        Post postToEdit = postRepository.findById(postId).orElseThrow(() -> new NotFoundException("해당 게시글이 존재하지 않습니다."));
+        List<PostImage> existingPostImages = postToEdit.getPostImageList();
+
+        List<PostImage> addedImages = newPostImages.stream()
+                .filter(image -> !existingPostImages.contains(image))
+                .toList();
+
+        List<PostImage> removedImages = existingPostImages.stream()
+                .filter(image -> !newPostImages.contains(image))
+                .toList();
+
+        addedImages.forEach(postToEdit::addPostImage);
+        removedImages.forEach(postToEdit::removePostImage);
+
+        return removedImages;
+    }
+
+}
