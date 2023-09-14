30364d213730972faa89801c4068cb9d659c8170
KimMinhee <99alsgmlalsl@naver.com>
2023-09-14 17:48:40 +0900

    [Feat] : PostImage entity 클래스 수정 - BaseEntity extends 추가 - fileName 필드 추가

--- src/main/java/com/daangn/dangunmarket/domain/post/model/PostImage.java
+++ src/main/java/com/daangn/dangunmarket/domain/post/model/PostImage.java
@@ -1,6 +1,15 @@
 package com.daangn.dangunmarket.domain.post.model;
 
-import jakarta.persistence.*;
+import com.daangn.dangunmarket.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +20,7 @@ import org.springframework.util.Assert;
 @Entity
 @Table(name = "post_images")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class PostImage {
+public class PostImage extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,13 +29,17 @@ public class PostImage {
     @Column(nullable = false)
     private String url;
 
+    @Column(nullable = false)
+    private String fileName;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "posts_id", referencedColumnName = "id")
     private Post post;
 
-    public PostImage(String url) {
+    public PostImage(String url, String fileName) {
         Assert.notNull(url, "url은 null값이 들어올 수 없습니다.");
 
+        this.fileName = fileName;
         this.url = url;
     }
 
