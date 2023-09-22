4ca1a7b0820b44b873cafb398eec35a10ee1efed
byeolhaha <j35635aa@gmail.com>
2023-09-22 17:35:04 +0900

    [Feat] : MongoDB 관련 세팅정보

---
+++ src/main/java/com/daangn/dangunmarket/global/config/MongoDbConfig.java
@@ -0,0 +1,39 @@
+package com.daangn.dangunmarket.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Configuration
+@EnableMongoAuditing
+@EnableMongoRepositories("com.daangn.dangunmarket.domain.chat.repository.chatmessage")
+public class MongoDbConfig {
+
+    @Bean
+    public MappingMongoConverter mappingMongoConverter(
+            MongoDatabaseFactory mongoDatabaseFactory,
+            MongoMappingContext mongoMappingContext
+    ) {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDatabaseFactory);
+        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return converter;
+    }
+
+    @Bean
+    public AuditorAware<LocalDateTime> auditorProvider() {
+        return () -> Optional.of(LocalDateTime.now());
+    }
+
+}
