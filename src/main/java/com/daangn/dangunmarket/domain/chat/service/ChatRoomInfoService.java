67aebdc8bf181ec2ad705e3f242e1397d733eb9f
byeolhaha <j35635aa@gmail.com>
2023-09-22 17:31:18 +0900

    [Feat] : 채팅방 관련 게시글 id와 상대방 정보를 불러오는 메서드 구현

--- src/main/java/com/daangn/dangunmarket/domain/chat/service/ChatRoomInfoService.java
+++ src/main/java/com/daangn/dangunmarket/domain/chat/service/ChatRoomInfoService.java
@@ -3,9 +3,11 @@ package com.daangn.dangunmarket.domain.chat.service;
 import com.daangn.dangunmarket.domain.chat.model.ChatRoomInfo;
 import com.daangn.dangunmarket.domain.chat.repository.chatroominfo.ChatRoomInfoRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Transactional
 @Service
 public class ChatRoomInfoService {
 
@@ -16,8 +18,16 @@ public class ChatRoomInfoService {
     }
 
     public boolean isExistedRoom(Long postId, Long memberId) {
-       Optional<ChatRoomInfo> chatRoomInfo = chatRoomInfoRepository.findChatRoomInfoByPostIdAndMemberId(postId,memberId);
-       return chatRoomInfo.isPresent();
+        Optional<ChatRoomInfo> chatRoomInfo = chatRoomInfoRepository.findChatRoomInfoByPostIdAndMemberId(postId, memberId);
+        return chatRoomInfo.isPresent();
+    }
+
+    public Long findSenderIdByChatRoomInfoAndMyId(Long chatRoomId, Long memberId) {
+        return chatRoomInfoRepository.findSenderIdByChatRoomInfoAndMyId(chatRoomId, memberId);
+    }
+
+    public Long findPostIdByChatRoomId(Long chatRoomId) {
+        return chatRoomInfoRepository.findPostIdByChatRoomId(chatRoomId);
     }
 
 }
