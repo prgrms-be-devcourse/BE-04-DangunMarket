3315e30f743cb9814e7ad08a013b77ccb5e926ef
byeolhaha <j35635aa@gmail.com>
2023-09-22 17:28:32 +0900

    [Feat] : 채팅방 입장시 일음처리, Redis 저장, post 및 상대방 관련 정보를 응답으로 보내주는 퍼사드 서비스 구현

--- src/main/java/com/daangn/dangunmarket/domain/chat/facade/ChatRoomFacade.java
+++ src/main/java/com/daangn/dangunmarket/domain/chat/facade/ChatRoomFacade.java
@@ -1,9 +1,14 @@
 package com.daangn.dangunmarket.domain.chat.facade;
 
 import com.daangn.dangunmarket.domain.chat.exception.RoomNotCreateException;
+import com.daangn.dangunmarket.domain.chat.facade.dto.ChatRoomCheckInParamResponse;
+import com.daangn.dangunmarket.domain.chat.facade.mapper.ChatRoomParamDtoMapper;
+import com.daangn.dangunmarket.domain.chat.service.ChatRoomEntryService;
 import com.daangn.dangunmarket.domain.chat.service.ChatRoomInfoService;
 import com.daangn.dangunmarket.domain.chat.service.dto.ChatRoomCreateRequest;
 import com.daangn.dangunmarket.domain.chat.service.ChatRoomService;
+import com.daangn.dangunmarket.domain.member.service.MemberService;
+import com.daangn.dangunmarket.domain.member.service.dto.MemberFindResponse;
 import com.daangn.dangunmarket.domain.post.service.PostService;
 import com.daangn.dangunmarket.domain.post.service.dto.PostFindResponse;
 import org.springframework.stereotype.Service;
@@ -16,21 +21,41 @@ public class ChatRoomFacade {
     private final PostService postService;
     private final ChatRoomService chatRoomService;
     private final ChatRoomInfoService chatRoomInfoService;
+    private final MemberService memberService;
+    private final ChatRoomParamDtoMapper chatRoomParamDtoMapper;
+    private final ChatRoomEntryService chatEntryService;
 
-    public ChatRoomFacade(PostService postService, ChatRoomService chatRoomService, ChatRoomInfoService chatRoomInfoService) {
+    public ChatRoomFacade(PostService postService, ChatRoomService chatRoomService, ChatRoomInfoService chatRoomInfoService,
+                          MemberService memberService, ChatRoomParamDtoMapper chatRoomParamDtoMapper, ChatRoomEntryService chatEntryService) {
         this.postService = postService;
         this.chatRoomService = chatRoomService;
         this.chatRoomInfoService = chatRoomInfoService;
+        this.memberService = memberService;
+        this.chatRoomParamDtoMapper = chatRoomParamDtoMapper;
+        this.chatEntryService = chatEntryService;
     }
 
-    public Long createChatRoom(Long memberId, ChatRoomCreateRequest request){
+    public Long createChatRoom(ChatRoomCreateRequest request){
 
-        if(chatRoomInfoService.isExistedRoom(request.postId(),memberId)) {
+        if(chatRoomInfoService.isExistedRoom(request.postId(),request.memberId())) {
             throw new RoomNotCreateException(NOT_CREATE_CHAT_ROOM);
         }
 
         PostFindResponse postFindResponse = postService.findById(request.postId());
-        return chatRoomService.createChatRoom(memberId,postFindResponse.memberId(),request);
+        return chatRoomService.createChatRoom(postFindResponse.memberId(),request);
+    }
+
+    public ChatRoomCheckInParamResponse checkInChatRoom(Long chatRoomId, Long memberId) {
+        Long senderId = chatRoomInfoService.findSenderIdByChatRoomInfoAndMyId(chatRoomId, memberId);
+        chatRoomService.readAllMessage(chatRoomId, senderId);
+
+        Long postId = chatRoomInfoService.findPostIdByChatRoomId(chatRoomId);
+        PostFindResponse postFindResponse = postService.findById(postId);
+        MemberFindResponse memberFindResponse = memberService.findById(senderId);
+
+        chatEntryService.addMemberToRoom(chatRoomId.toString(),memberId.toString());
+
+        return chatRoomParamDtoMapper.toChatRoomCheckInParamResponse(postFindResponse,memberFindResponse);
     }
 
 }
