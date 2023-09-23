892fe0ebda26f63eb9a53fa711f1e4576e3fceeb
byeolhaha <j35635aa@gmail.com>
2023-09-23 16:54:11 +0900

    [Refactor] : 해석하기 어려운 삼항 연산자를 메서드로 분리

--- src/main/java/com/daangn/dangunmarket/domain/post/controller/annotation/validator/FileValidator.java
+++ src/main/java/com/daangn/dangunmarket/domain/post/controller/annotation/validator/FileValidator.java
@@ -7,15 +7,38 @@ import jakarta.validation.ConstraintValidatorContext;
 
 public class FileValidator implements ConstraintValidator<FileSize, PostImageUpdateApiRequest> {
 
+    private static final int MAX_IMAGE_FILE_COUNT = 3;
+
     @Override
     public boolean isValid(PostImageUpdateApiRequest request, ConstraintValidatorContext context) {
         if (request == null) {
             return true;
         }
 
-        int totalFileCount = (request.files() != null ? request.files().size() : 0) +
-                (request.urls() != null ? request.urls().size() : 0);
+        int totalFileCount = fileCount(request) + urlCount(request);
 
-        return totalFileCount < 4;
+        if (totalFileCount > MAX_IMAGE_FILE_COUNT) {
+            context.buildConstraintViolationWithTemplate("사진의 개수가 너무 많습니다.").addPropertyNode("files");
+            return false;
+        }
+
+        return true;
     }
+
+    private int urlCount(PostImageUpdateApiRequest request) {
+        if (request.urls() == null) {
+            return 0;
+        }
+
+        return request.urls().size();
+    }
+
+    private int fileCount(PostImageUpdateApiRequest request) {
+        if (request.files() == null) {
+            return 0;
+        }
+
+        return request.files().size();
+    }
+
 }
